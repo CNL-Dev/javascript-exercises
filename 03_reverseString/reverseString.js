f3548f48a483906fb2da45759150cae2d27b00f3
Caleb Lugo <cnl1211999@gmail.com>
2023-09-05 15:36:35 -0700

    Solved exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+    let reversedString = "";
 
+    for(let i = inputString.length; i > -1; i--){
+        reversedString = reversedString.concat("",inputString.charAt(i));
+    }
+
+    return reversedString;
 };
 
 // Do not edit below this line
