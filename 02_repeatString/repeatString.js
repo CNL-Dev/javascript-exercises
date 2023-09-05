46134698b99001ad7f4f0d2747a06bd033acc0c0
Caleb Lugo <cnl1211999@gmail.com>
2023-09-05 15:29:27 -0700

    Solved exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+    if(num < 0){
+        return "ERROR";
+    }
 
+    let repeatedWord = "";
+
+    for(let i = 0; i < num; i++){
+        repeatedWord = repeatedWord.concat("", word);
+    }
+
+    return repeatedWord;
 };
 
 // Do not edit below this line
