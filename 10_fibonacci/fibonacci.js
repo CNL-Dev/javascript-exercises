d18047d994ac305de12a409553bc10d8fd023ef1
Caleb Lugo <cnl1211999@gmail.com>
2023-09-25 16:12:38 -0700

    Solved exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function(inputNum) {
+    // Converts string to number if possible
+    let num = +inputNum;
+    // Base cases
+    if(num < 0) return "OOPS";
+    if(num <= 1) return num;
 
+    return fibonacci(num - 1) + fibonacci(num - 2);
 };
 
 // Do not edit below this line
