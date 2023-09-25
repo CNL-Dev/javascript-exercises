e09bc9d258474b3c2d7128c638fd8b6b87ff1b91
Caleb Lugo <cnl1211999@gmail.com>
2023-09-25 15:56:31 -0700

    Solved exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (inputString) {
+    let string = inputString.toLowerCase();
+    // Removes all punctuation and whitespace
+    const filteredString = string.replace(/[^\w\s']|_/g, "")
+                                 .replace(/\s+/g, "");
 
+    function reverseString(string) {
+        let reversedString = "";
+        for(let i = string.length - 1; i > -1; i--){
+            reversedString += string.charAt(i);
+        }
+        console.log(reversedString);
+        return reversedString;
+    }
+
+    return filteredString == reverseString(filteredString);
 };
 
 // Do not edit below this line
